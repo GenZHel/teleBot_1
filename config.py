d39b6b0887fdf3bd19a9536e5300645e258cb10a
GenZHel <ivanvylegzhanin.1997@gmail.com>
2022-10-27 12:49:31 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+
+TOKEN = "5751381129:AAFAcfYL6iAGZjK7hnSYUe74cqGkpzggTSo"
+
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
---
+++ extensions.py
@@ -0,0 +1,36 @@
+import requests
+import json
+from config import keys
+
+
+class ConvertionException(Exception):
+    pass
+
+
+class ValueConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: float):
+
+        if quote == base:
+            raise ConvertionException(f'Невозможно перевести "{quote}" в "{base}"!')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту "{quote}"')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту "{base}"')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f'Не удалось обработать валюту "{amount}"')
+
+        r = requests.get(f'https://api.tinkoff.ru/v1/currency_rates?from={quote_ticker}&to={base_ticker}')
+        text = json.loads(r.content)['payload']['rates']
+        dict_1 = text[0]
+        price = round(dict_1['buy'] * amount, 5)
+        return price
